ce0078a946fb5b312ee4663d348c5bfc90257f62
amargozz <malexgope@gmail.com>
2025-11-27 19:41:29 -0600

    Delete mediaplay directory

--- mediaplay/mediaplay_func.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getMedia($folder = 'KIOSK_MEDIA') {                                     //Path of directory
-    $imagenes = glob("$folder/*.{jpg,jpeg,png,PNG,gif,bmp,webp}", GLOB_BRACE);
-    $videos = glob("$folder/*.{mp4,webm,ogg}", GLOB_BRACE);
-    return array_merge($imagenes, $videos);
-}
-?>
-
-
